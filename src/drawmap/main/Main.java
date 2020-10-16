9b7a151d09d2506086ca1439fb51a95327e2fb26
Florian <florian.aymard@gmail.com>
2020-10-16 11:27:18 +0200

    Amélioration de la génération du graphe en mémoire

--- src/drawmap/main/Main.java
+++ src/drawmap/main/Main.java
@@ -5,7 +5,6 @@ import drawmap.view.*;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
--- src/drawmap/model/Intersection.java
+++ src/drawmap/model/Intersection.java
@@ -1,10 +1,14 @@
 package drawmap.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Intersection {
 
     private Long id;
     private Double latitude;
     private Double longitude;
+    private ArrayList<Pair<Segment, Intersection>> voisins;
 
     public Intersection() {
     }
@@ -37,5 +41,38 @@ public class Intersection {
 
     public void setLongitude(Double longitude) {
         this.longitude = longitude;
+    }
+
+    public void addVoisin(Segment s, Intersection i) {
+        if(voisins.isEmpty()) {
+            voisins.add(new Pair<Segment, Intersection>(s,i));
+            return;
+        }
+        Iterator it = voisins.iterator();
+
+        int index = -1;
+        for(int j = 0; j < voisins.size(); ++j) {
+            Pair<Segment, Intersection> current = voisins.get(j);
+
+            if(current.getL().getLength() > s.getLength() && index == -1) {
+                index = j;
+            }
+
+            if(current.getL().getDestination().getId() == s.getDestination().getId()
+            && current.getL().getLength() < s.getLength()) {
+                index = -1;
+                break;
+            }
+            else if(current.getL().getDestination().getId() == s.getDestination().getId()
+                    && current.getL().getLength() > s.getLength()) {
+                voisins.remove(i);
+            }
+        }
+
+        if(index != -1) {
+            voisins.add(index, new Pair<Segment, Intersection>(s,i));
+        }
+
+
     }
 }
